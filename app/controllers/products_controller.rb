f07ad977a90898cd1d9bc36ed992b5cc1f9b12b6
brian <riceb53@gmail.com>
2023-08-02 10:40:08 -0700

    products destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,13 +24,20 @@ class ProductsController < ApplicationController
   def update
     # combination of show and create
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.description = params[:description]
-    @product.image_url = params[:image_url]
-    @product.price = params[:price]
+    @product.name = params[:name] || @product.name
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.price = params[:price] || @product.price
     @product.save
-    
     render :show
+  end
+
+  def destroy
+    # find the product
+    @product = Product.find_by(id: params[:id])
+    # remove the product
+    @product.destroy
+    render json: {message: "product removed"}
   end
   
 end
