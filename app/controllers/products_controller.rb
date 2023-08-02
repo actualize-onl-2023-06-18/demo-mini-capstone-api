ddfd484db1499dd087699b775ee915ecfe7d1fa8
brian <riceb53@gmail.com>
2023-08-02 10:26:00 -0700

    dynamic products create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,17 @@ class ProductsController < ApplicationController
     render :index
   end
 
+  def create
+    @product = Product.new(
+      image_url: params[:image_url], 
+      name: params[:name], 
+      price: params[:price], 
+      description: params[:description]
+    )
+    @product.save
+    render :show
+  end
+  
   def show
     # return one product
     @product = Product.find_by(id: params[:id])
