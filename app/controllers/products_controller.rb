8f987cb30b3bf338844966eb93aeec9fdcc35082
brian <riceb53@gmail.com>
2023-08-02 09:55:14 -0700

    products show action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,10 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+
+  def show
+    # return one product
+    @product = Product.find_by(id: params[:id])
+    render :show
+  end
 end
