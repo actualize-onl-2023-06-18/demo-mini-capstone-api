fa76477efe50210f91424df2d19def8e4a5982ff
brian <riceb53@gmail.com>
2023-08-03 11:34:55 -0700

    add model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,15 @@
 class Product < ApplicationRecord
   def is_discounted?
-    if price <= 10
-      return true
-    else
-      return false
-    end    
+    price <= 10    
   end
+
+  def tax
+    tax_rate = 0.09
+    price * tax_rate
+  end
+
+  def total
+    price + tax
+  end
+  
 end
