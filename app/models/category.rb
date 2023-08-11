351f1c8c0bfe6bbe743e9abcafa373b60c7269e5
brian <riceb53@gmail.com>
2023-08-11 10:28:44 -0700

    add has_many through relationships for category and product

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,11 +1,12 @@
 class Category < ApplicationRecord
   has_many :category_products
+  has_many :products, through: :category_products
 
-  def products
-    # get to join table
-    category_products.each do |cp|
-      p cp.product
-    end
+  # def products
+  #   # get to join table
+  #   category_products.each do |cp|
+  #     p cp.product
+  #   end
     # go to each, find product_id, find the right product
-  end
+  # end
 end
