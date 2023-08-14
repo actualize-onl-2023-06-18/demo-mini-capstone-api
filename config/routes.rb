4fb1e8e9dd53b2ac61b823f9aa2c46b790a811b6
brian <riceb53@gmail.com>
2023-08-14 13:23:23 -0700

    carted products create, show only items with correct status and user id

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
   
+  get "/carted_products" => "carted_products#index"
   post "/carted_products" => "carted_products#create"
 end
