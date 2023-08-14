830d3430eff61d5e5bb8c4badf38dce4da2b6d9d
brian <riceb53@gmail.com>
2023-08-14 13:16:56 -0700

    associate carted product, order and product, make carted products create

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,8 @@ class Product < ApplicationRecord
 
   has_many :category_products
   has_many :categories, through: :category_products
+
+  has_many :carted_products  
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
